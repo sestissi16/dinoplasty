2506349daf582da0101ffb30ede5bdf41548aa07
Sari Stissi <sestissi16@earlham.edu>
2017-06-07 18:00:49 -0400

    Setup submit handler for dino form

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = {
+    init(formSelector) {
+        document
+            .querySelector(formSelector)
+            .addEventListener('submit', this.addDino)
+    },
+
+    addDino(ev) {
+        ev.preventDefault()
+        const dinoName = ev.target.dinoName.value
+        console.log(dinoName)
+    },
+}
+
+app.init('#dino-form')
