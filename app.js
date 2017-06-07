4a9a5b99ad3c60a354f1cc63dd725d72cad5ad97
Sari Stissi <sestissi16@earlham.edu>
2017-06-07 19:38:10 -0400

    Remove bullets from list and added to buttons to each list item

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = {
     init(selectors) {
         this.max = 0
+        this.dinos = []
         this.list = document.querySelector(selectors.listSelector)
         document
             .querySelector(selectors.formSelector)
@@ -18,14 +19,24 @@ const app = {
         const listItem = this.renderListItem(dino)
         this.list.appendChild(listItem)
 
+        this.dinos.push(dino)
+
         ++ this.max
     },
 
     renderListItem(dino) {
         const item = document.createElement('li')
         item.textContent = dino.name
+        item.innerHTML += `
+            <div class="button-group">
+                <a class="warning button">Favorite</a>
+                <a class="alert button">Delete</a>
+            </div>
+        `
+        //deleteButton = $('<span />').addClass('')
         return item
     },
+
 }
 
 app.init({
