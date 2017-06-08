0e2ea2a69d88b2448e086cd0468d300650d64fd1
Sari Stissi <sestissi16@earlham.edu>
2017-06-08 12:22:42 -0400

    Add attempt at getting up and down buttons to work

--- app.js
+++ app.js
@@ -49,12 +49,50 @@ const app = {
                 >
                     Delete
                 </button>
+                <button
+                    id="upBtn"
+                    class="button"
+                    onclick="app.move()"
+
+                >
+                    Up
+                </button>
+                <button
+                    id="downBtn"
+                    class="button"
+                    onclick="app.move()"
+                >
+                    Down
+                </button>
             </div>
         `
+        
         return item
     },
+    
+    move() {
+        debugger
+        const ul = document.getElementById('dino-list')
+        const items = ul.getElementsByTagName('li')
+
+        this.counter = 0
+        this.previousItem = null
+        this.moveNextItemUp = false
+
+        for (let i = 0; i<items.length; i++){
+            if(this.id === 'downBtn'){
+                this.moveNextItemUp = true
+            }
+            else if ((this.id === 'upBtn') || (this.moveNextItemUp === true)){
+                items[i].parentNode.insertBefore(item[i], items[i-1])
+                break
+            }
+            this.previousItem = items[i]
+            this.counter += 1
+        }
 
 
+    },
     
 
 }
